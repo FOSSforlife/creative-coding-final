4c92f8db7091551b228445581af8b9bc7a3141ec
Elias Jackson <ejthedj0@gmail.com>
2025-03-19 11:08:34 -0700

    Update

--- sketch.js
+++ sketch.js
@@ -5,16 +5,16 @@ let bg;
 let font;
 
 function preload() {
-  // font = loadFont('/assets/inconsolata.otf');
+  font = loadFont('/assets/Tektur-Regular.ttf');
 }
 
 function setup() {
   createCanvas(853, 480);
 
   // fill('deeppink');
-  // textFont(font);
+  textFont(font);
   // textSize(36);
-  // text('Elias's Music Collection', 10, 50);
+  text("Elias's Music Collection", 10, 50);
 
   libraryPage = new LibraryPage();
   bg = new Background();
@@ -55,13 +55,20 @@ function mouseClicked() {
 
 class Background {
   constructor() {
-    this.bubbles = new Array(8).fill(0).map(() => new BackgroundBubble());
+    this.bubbles = new Array(16).fill(0).map(() => new BackgroundBubble());
     this.transitionState = 'none'; // none, init (up), off-screen (left), onto-screen (right)
     this.transitionProgress = 0.0;
   }
   update() {
     for (const bubble of this.bubbles) {
-      bubble.update(this.transitionProgress);
+      if (this.transitionProgress >= 1) {
+        this.transitionProgress = 0;
+        this.transitionState = 'none';
+      } else if (this.transitionState !== 'none') {
+        // console.log(this.transitionProgress + ' ' + this.transitionState);
+        this.transitionProgress += 1 / 600;
+      }
+      bubble.update(this.transitionState, this.transitionProgress);
     }
   }
   draw() {
@@ -78,9 +85,11 @@ function transitionVelocity(progress, maxVelocity) {
     : map(progress, 0.5, 1, maxVelocity, 0);
 }
 
-function switchToAlbumPage(albumPageColor) {
-  bg.transitionState = 'slide-left';
-  bg.albumPageColor = albumPageColor;
+function switchToAlbumPage(album) {
+  albumPage = new AlbumPage(album);
+  currentView = 'album';
+  bg.transitionState = 'off-screen';
+  // bg.albumPageColor = album.color;
 }
 
 function interpNorm(amount) {
@@ -93,20 +102,19 @@ function interpPoint(x1, x2, amount) {
 
 class BackgroundBubble {
   constructor() {
-    this.mainPosition = createVector(random(width), random(height));
-    this.position = this.mainPosition;
+    this.mainPosition = createVector(random(width * 2), random(height));
+    this.position = this.mainPosition.copy();
     this.zPos = random(-90, 0);
     this.radius = 120 + this.zPos;
-    this.transitionState = 'none'; // none, init (up), slide-left, slide-right
-    this.transitionProgress = 0.0;
     this.mainColor = color(30, 110, random(180, 255));
     this.albumPageColor = color(200, 100, 30);
   }
 
-  update(transitionProgress) {
+  update(transitionState, transitionProgress) {
     let maxVelocity;
+    let targetX;
 
-    switch (this.transitionState) {
+    switch (transitionState) {
       case 'none':
         this.position.add(this.velocity);
         break;
@@ -117,19 +125,24 @@ class BackgroundBubble {
         );
         break;
       case 'off-screen':
-        const targetX =
+        targetX =
           this.mainPosition.x -
-          (width / 2 - map(1 / zPos, 0, 0.5, 0, width / 4));
-        this.mainPosition.x = interpPoint(
+          (width / 2 - map(1 / this.zPos, 0, 0.5, 0, width / 4));
+        this.position.x = interpPoint(
           this.mainPosition.x,
           targetX,
           transitionProgress
         );
+        console.log({ targetX, positionX: this.position.x });
         break;
       case 'onto-screen':
-        maxVelocity = 50; // TODO: calculate based on distance and frames
-        this.position.add(
-          createVector(-transitionVelocity(transitionProgress, maxVleocity), 0)
+        targetX =
+          this.mainPosition.x -
+          (width / 2 - map(1 / this.zPos, 0, 0.5, 0, width / 4));
+        this.position.x = interpPoint(
+          this.mainPosition.x,
+          targetX,
+          transitionProgress
         );
         break;
     }
@@ -148,6 +161,10 @@ class LibraryPage {
       {
         artist: 'Animals as Leaders',
         title: 'The Joy of Motion',
+        color: color(200, 50, 0),
+        youtube:
+          'https://www.youtube.com/watch?v=mLPLhULHeXw&list=OLAK5uy_l7Cxjtvtra-8dZqY6fkSD1SOekHd-aKAQ',
+        Descriptors: [''],
       },
       {
         artist: 'Animals as Leaders',
@@ -163,25 +180,136 @@ class LibraryPage {
       },
     ];
     this.resultsComponent = new ResultsComponent(this.results);
+    this.availableTags = ['adventurous', 'futuristic', 'fun', 'harsh'];
+    this.selectedTags = [];
+    this.queryComponent = new QueryComponent(
+      this.availableTags,
+      this.selectedTags,
+      (filterObj) => this.toggleDescriptor(filterObj)
+    );
   }
 
-  query(filters) {
-    this.results = this.results.filter(() => true);
+  toggleDescriptor(descriptorText) {
+    if (this.selectedTags.some((name) => name === descriptorText)) {
+      this.selectedTags = this.selectedTags.filter(
+        (name) => name !== descriptorText
+      );
+      // print(this.selectedTags);
+    } else {
+      this.selectedTags.push(descriptorText);
+    }
+    this.updateResults();
+  }
+
+  updateResults() {
+    this.results = this.results.filter(
+      (album) =>
+        album.Descriptors &&
+        album.Descriptors.every((descriptor) =>
+          this.selectedTags.includes(descriptor)
+        )
+    );
+    this.queryComponent.createComponents(this.selectedTags);
   }
 
   mouseClicked() {
     this.resultsComponent.mouseClicked();
+    this.queryComponent.mouseClicked();
   }
 
   update() {
     this.resultsComponent.update();
+    this.queryComponent.update();
   }
   draw() {
     this.resultsComponent.draw();
+    this.queryComponent.draw();
   }
 }
 
-class QueryComponent {}
+class QueryComponent {
+  constructor(availableTags, selectedTags, toggleFn) {
+    this.tags = availableTags;
+    this.selectedTags = selectedTags;
+    this.tagComponents = [];
+    this.toggleFn = toggleFn;
+    this.createComponents(this.selectedTags);
+  }
+
+  createComponentsDebug() {
+    print({ tags: this.tags });
+    print({ selectedTags: this.selectedTags });
+  }
+
+  createComponents(selectedTags) {
+    // print({tags: this.tags});
+    // print({selectedTags: this.selectedTags});
+    let x = 50;
+    let y = 50;
+    this.tagComponents = [];
+    for (const descriptorText of this.tags) {
+      const bbox = font.textBounds(descriptorText, x, y, 20);
+      const selected = selectedTags.some((name) => name === descriptorText);
+      this.tagComponents.push({ descriptorText, x, y, bbox, selected });
+      x += bbox.w;
+      x += 15;
+      if (x > width - 150) {
+        x = 50;
+        y += 30;
+      }
+    }
+  }
+
+  static itemDimensions(index) {
+    const margin = {
+      x: 50,
+      y: 50,
+    };
+    const itemSize = {
+      x: 100,
+      y: 20,
+    };
+    const spacing = 15;
+    const rowSize = (width - 2 * margin.x) / (itemSize.x + spacing);
+
+    const itemRow = index % rowSize;
+    const itemColumn = Math.floor(index / rowSize);
+    const x = margin.x + itemRow * (itemSize.x + spacing);
+    const y = margin.y + itemColumn * (itemSize.y + spacing);
+    return { itemRow, itemColumn, itemSize, x, y };
+  }
+
+  mouseClicked() {
+    // print('mouse clicked on query component')
+    // print({mouseX, mouseY, tags: this.tagComponents});
+    for (const { bbox, descriptorText } of this.tagComponents) {
+      if (
+        mouseX >= bbox.x &&
+        mouseX <= bbox.x + bbox.w &&
+        mouseY >= bbox.y &&
+        mouseY <= bbox.y + bbox.h
+      ) {
+        // print(`mouse clicked on ${descriptorText}`);
+        this.toggleFn(descriptorText);
+      }
+    }
+  }
+
+  update() {}
+  draw() {
+    for (const { descriptorText, x, y, bbox, selected } of this.tagComponents) {
+      push();
+      const boxColor = selected ? color(60, 180, 180) : color(60, 180, 60);
+      fill(boxColor);
+      noStroke();
+      rect(bbox.x, bbox.y, bbox.w, bbox.h);
+      stroke(255);
+      fill(255);
+      text(descriptorText, x, y);
+      pop();
+    }
+  }
+}
 
 class ResultsComponent {
   constructor(results) {
@@ -197,8 +325,7 @@ class ResultsComponent {
         mouseY > y &&
         mouseY < y + itemSize
       ) {
-        albumPage = new AlbumPage(this.results[i]);
-        currentView = 'album';
+        switchToAlbumPage(this.results[i]);
       }
     }
   }
@@ -206,7 +333,7 @@ class ResultsComponent {
   static itemDimensions(index) {
     const margin = {
       x: 50,
-      y: 50,
+      y: 150,
     };
     const itemSize = 100;
     const spacing = 30;
@@ -285,7 +412,7 @@ class Polygon {
   }
 
   draw() {
-    fill(color(this.hue, 80, 100));
+    fill(color(200, 80, 100));
     push();
     translate(this.position.x, this.position.y);
     rotate(this.rotation);
